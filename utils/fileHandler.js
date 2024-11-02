d1bb0d9182c43a033772ab025091654038723afe
Your Name <your.email@example.com>
2024-11-01 20:44:31 -0700

    adding tasks

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const filePath = './data/tasks.json';
 
-const writeTasksToFile = (tasks) => {
+exports.writeTasksToFile = (tasks) => {
     fs.writeFileSync(filePath, JSON.stringify(tasks,))
 }
-const readTasksFromFile = () => {
+exports.readTasksFromFile = () => {
     if(!fs.existsSync(filePath)) {
         writeTasksToFile([])
     }
